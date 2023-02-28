8467445cd1da073e1da514582f9f3bc6bd0f9521
Ademario Teles <ademario.teles@serpro.gov.br>
2023-02-28 13:36:02 -0300

    faturamento distribuidora final

--- faturamentoDistribuidora/src/application/Principal.java
+++ faturamentoDistribuidora/src/application/Principal.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Scanner;
-
 /******************************************************************************
  * Vaga para Estágio Job Rotation - São Paulo
  * 
@@ -23,33 +21,27 @@ public class Principal {
 	public static String faturamento(double SP, double RJ, double MG, double ES, double outros) {
 
 		double total = SP + RJ + MG + ES + outros;
-		return "Percentual de Faturamento de SP:" + String.format("%.2f",(SP / total) * 100) + "%" 
-				+ "\nPercentual de Faturamento de RJ:"  + String.format("%.2f",(RJ / total) * 100) + "%" 
-				+ "\nPercentual de Faturamento de MG:" + String.format("%.2f",(MG / total) * 100 )+ "%" 
-				+ "\nPercentual de Faturamento de ES:"  + String.format("%.2f",(ES / total) * 100) + "%" 
-				+ "\nPercentual de Faturamento dos outros estados:" + String.format("%.2f",(outros / total) * 100) + "%" ;
+		return "Percentual de Faturamento de SP:" + String.format("%.2f", (SP / total) * 100) + "%"
+				+ "\nPercentual de Faturamento de RJ:" + String.format("%.2f", (RJ / total) * 100) + "%"
+				+ "\nPercentual de Faturamento de MG:" + String.format("%.2f", (MG / total) * 100) + "%"
+				+ "\nPercentual de Faturamento de ES:" + String.format("%.2f", (ES / total) * 100) + "%"
+				+ "\nPercentual de Faturamento dos outros estados:" + String.format("%.2f", (outros / total) * 100)
+				+ "%";
 	}
 
 	public static void main(String[] args) {
 
-		Scanner entrada = new Scanner(System.in);
+		double SP = 67.83643;
 
-		System.out.printf("Digite o valor do faturamento de SP em R$:");
-		double SP = entrada.nextDouble();
+		double RJ = 36.67866;
 
-		System.out.printf("Digite o valor do faturamento do RJ em R$:");
-		double RJ = entrada.nextDouble();
+		double MG = 29.22988;
 
-		System.out.printf("Digite o valor do faturamento de MG em R$:");
-		double MG = entrada.nextDouble();
+		double ES = 27.16548;
 
-		System.out.printf("Digite o valor do faturamento do ES em R$:");
-		double ES = entrada.nextDouble();
+		double outros = 19.84953;
 
-		System.out.printf("Digite o valor do faturamento referente aos outros Estados em R$:");
-		double outros = entrada.nextDouble();
-		
-		System.out.println(faturamento(SP,RJ,MG,ES,outros));
+		System.out.println(faturamento(SP, RJ, MG, ES, outros));
 
 	}
 
